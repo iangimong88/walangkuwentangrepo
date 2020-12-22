2d230eb17084dad1ba7d60d74fabaa08fe1b986d
iangimong88 <76453240+iangimong88@users.noreply.github.com>
2020-12-23 01:10:04 +0800

    Update chk_live.php

--- chk_live.php
+++ chk_live.php
@@ -4,21 +4,29 @@ error_reporting(0);
 date_default_timezone_set('America/Sao_Paulo');
 
 
-extract($_GET);
+$botToken = "689347298:AAEcpBcIifMqdT0dmFBm2Jo6xI8LiiJvHOU";
+$botUrl = "https://api.telegram.org/bot".$botToken;
 
+$update = file_get_contents("php://input");
+$data = json_decode($update, TRUE);
 
-for ($i=0; $i <= substr_count($lista, ','); $i++) { 
-	$cclist = explode(",", $lista); 
-	$entry = explode("|", $cclist[$i]);
-	
-	$cc = $entry[0];
-	$mes = $entry[1];
-	$ano = $entry[2];
-	$cvv = $entry[3];
-	$email = "doublebrown".rand(1, 200000)."@gmail.com";
-	$postal = rand(0000, 9999);
+$chatId = $data["message"]["chat"]["id"];
+$message = $data["message"]["text"];
 
+switch($message){
+	case "/start":
+		sendMessage($chatId, "Maligayang pagdating sa aking bot. Ito ay para sa mga jolly batibot.");
+		break;
+	case "/test":
+		sendMessage($chatId, "This is test.");
+		break;
+	default:
+		sendMessage($chatId, "Hindi kita maunawaan.");
+}
 
+function sendMessage($chatId, $message){
+	$url = $GLOBALS[website]."/sendMessage?chat_id=" .$chatId. "&text=" .urlencode($message);
+	file_get_contents($url);
 }
 
 
