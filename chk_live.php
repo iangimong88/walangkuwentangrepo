8c631d1f8384e6e9671d27e698b7407b86e34296
iangimong88 <76453240+iangimong88@users.noreply.github.com>
2020-12-23 14:40:33 +0800

    Update chk_live.php

--- chk_live.php
+++ chk_live.php
@@ -25,9 +25,8 @@ switch($message){
 }
 
 function sendMessage($chatId, $message){
-	$url = $GLOBALS[website]."/sendMessage?chat_id=" .$chatId. "&text=" .urlencode($message);
+	$url = $GLOBALS["botUrl"]."/sendMessage?chat_id=" .$chatId. "&text=" .urlencode($message);
 	file_get_contents($url);
 }
 
-echo "connected.";
 ?>
